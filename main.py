35596b46adcc6c68e05ac55d8bec831e7647281a
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-01-16 12:58:52 -0600

    Update main.py
    More formatting, plus docstring

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# FIZZBUZZ Challenge
-# Language: Python
+"""
+FIZZBUZZ Challenge
+Language: Python
+Parameters: None
+Return: 
+    List of numbers 1 to 100 with 
+        numbers divisible by 3 replaced with FIZZ, 
+        numbers divisible by 5 replaced with BUZZ, and 
+        numbers replaceable by both 3 and 5 replaced with FIZZBUZZ.
+"""
 
 # define list NUMS
 nums = []
@@ -9,19 +17,19 @@ for i in range(1,100):
     # pre-define the bool values of FIZZ and BUZZ
     FIZZ = False
     BUZZ = False
-    
+
     #check if I is divisible by 3 with no remainder
     if i%3 == 0:
         FIZZ = True
-    
+
     # check if I is divisible by 5 with no remainder
     if i%5 == 0:
         BUZZ = True
-    
+
     # if FIZZ = false, 2 options remain: BUZZ and neither
-    if FIZZ == False:
+    if not FIZZ:
         # if BUZZ is false, then the number with neither FIZZ nor BUZZ
-        if BUZZ == False:
+        if not BUZZ:
             nums.append(i)
         # Otherwise, BUZZ is true, so append BUZZ
         else:
@@ -29,11 +37,11 @@ for i in range(1,100):
     # FIZZ is true, so its either FIZZ or FIZZBUZZ
     else:
         # If Buzz is false here, use FIZZ
-        if BUZZ == False:
+        if not BUZZ:
             nums.append("FIZZ")
         # Otherwise, append FIZZBUZZ
         else:
             nums.append("FIZZBUZZ")
 
-# Print out the final result      
+# Print out the final result
 print(nums)
