fbd7218bf61952be2c82d8f9f355c24c163088b8
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-01-16 12:50:58 -0600

    Update main.py
    Formatting

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ nums = []
 # iterate through range of numbers
 for i in range(1,100):
     # pre-define the bool values of FIZZ and BUZZ
-    fizz = False
-    buzz = False
-
+    FIZZ = False
+    BUZZ = False
+    
     #check if I is divisible by 3 with no remainder
     if i%3 == 0:
-        fizz = True
-
+        FIZZ = True
+    
     # check if I is divisible by 5 with no remainder
     if i%5 == 0:
-        buzz = True
-
+        BUZZ = True
+    
     # if FIZZ = false, 2 options remain: BUZZ and neither
-    if fizz == False:
+    if FIZZ == False:
         # if BUZZ is false, then the number with neither FIZZ nor BUZZ
-        if buzz == False:
+        if BUZZ == False:
             nums.append(i)
         # Otherwise, BUZZ is true, so append BUZZ
         else:
             nums.append("BUZZ")
-        # FIZZ is true, so its either FIZZ or FIZZBUZZ
-        else:
+    # FIZZ is true, so its either FIZZ or FIZZBUZZ
+    else:
         # If Buzz is false here, use FIZZ
-        if buzz == False:
+        if BUZZ == False:
             nums.append("FIZZ")
         # Otherwise, append FIZZBUZZ
         else:
